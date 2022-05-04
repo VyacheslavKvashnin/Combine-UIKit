3c232e6aa74250fb2e474ce756b051689488b78f
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-04 23:08:48 +0500

    Finish project

--- Combine&UIKit/ViewController.swift
+++ Combine&UIKit/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
@@ -15,16 +16,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var postLabel: UILabel!
     
+    @Published var isShowButton: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        $isShowButton
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: pressedButtonLabel)
+        
+        setupCombine()
     }
 
-    @IBAction func showButtonSwitch(_ sender: Any) {
+    @IBAction func showButtonSwitch(_ sender: UISwitch) {
+        isShowButton = sender.isOn
     }
     
     @IBAction func pressedButton(_ sender: Any) {
+        let blogPost = BlogPost(title: "Some Titel\n \(Date())")
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
+    }
+    
+    private func setupCombine() {
+        let blogPostPublisher = NotificationCenter.Publisher(
+            center: .default,
+            name: .newBlogPost,
+            object: nil
+        )
+            .map { notification -> String? in
+                return (notification.object as? BlogPost)?.title
+            }
+        
+        let postSubscriber = Subscribers.Assign(object: postLabel, keyPath: \.text)
+        blogPostPublisher
+            .subscribe(postSubscriber)
     }
 }
 
+struct BlogPost {
+    let title: String
+}
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newBlogPost")
+}
